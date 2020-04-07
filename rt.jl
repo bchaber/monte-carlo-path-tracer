0ddbbe979f85d06b2563054622c96896d9a32a40
Bartosz Chaber <bartosz.chaber@ee.pw.edu.pl>
2020-04-07 22:33:03 +0200

    Adjust the code to use Dual numbers

--- rt.jl
+++ rt.jl
@@ -1,25 +1,33 @@
 module Raytracer
     import Printf: @printf
+    include("dual.jl")
 
-    Tuple1 = Tuple{Float64}
-    Tuple3 = Tuple{Float64, Float64, Float64}
+    Float  = Float64#Dual{Float64}
+    Tuple1 = Tuple{Float}
+    Tuple3 = Tuple{Float, Float, Float}
+
+    abstract type Material end
+    struct Diffusive  <: Material end
+    struct Specular   <: Material end
 
     struct Hit
-        distance  :: Float64
+        distance  :: Float
         direction :: Tuple3
     end
 
     struct Sphere
-        radius  :: Float64
+        radius  :: Float
         center  :: Tuple3
         emission :: Tuple1
         color :: Tuple1
+        material :: Material
     end
+    corrected(x::Dual) = 255.0clamp(x)^(1.0/2.2) + .5
     corrected(x::Number) = floor(UInt8, (255.0clamp(x)^(1.0/2.2) + .5))
-    normalize(v::Tuple3) = v./sqrt(dot(v,v))
+    normalize(v) = v./sqrt(dot(v,v))
     clamp(x::Number) = x < zero(x) ? zero(x) : x > one(x) ? one(x) : x
-    cross(u::Tuple3, v::Tuple3) = (u[2]*v[3]-u[3]*v[2], u[3]*v[1]-u[1]*v[3],u[1]*v[2]-u[2]*v[1])
-    dot(u::Tuple3,v::Tuple3) = sum(u .* v)
+    cross(u, v) = (u[2]*v[3]-u[3]*v[2], u[3]*v[1]-u[1]*v[3],u[1]*v[2]-u[2]*v[1])
+    dot(u,v) = sum(u .* v)
 
 const lights = (
   Sphere(600., (50,681.6-.57,81.6),(0.25,), (.99,)),
@@ -42,7 +50,7 @@ const spheres = (
       hit = nothing
       for sph=(spheres..., lights...)
         d = intersect(origin, direction, sph)
-        if d > 0. && d < t
+        if d > zero(d) && d < t*one(d)
           t = d
           hit = sph
         end
@@ -54,22 +62,22 @@ const spheres = (
       op = sph.center .- origin
       b  = dot(op, direction)
       det = b*b - dot(op, op) + sph.radius*sph.radius
-      if det < 0. return 0.0 end
+      if det < zero(det) return zero(det) end
       det = sqrt(det)
-      if (b - det) > 1e-4 return b - det end
-      if (b + det) > 1e-4 return b + det end
-      return 0.
+      if (b - det) > 1e-4one(det) return b - det end
+      if (b + det) > 1e-4one(det) return b + det end
+      return zero(det)
     end
 
     function raytrace(origin, direction)
       t, hit = intersect(origin, direction)
       if hit == nothing
-          return (0.,)
+          return (zero(t),)
       end
-      f = (0.,)
+      f = (zero(t),)
       x = origin .+ t .* direction
       n = normalize(x .- hit.center)
-      nl = dot(n, direction) < 0.0 ? n : -1.0.*n
+      nl = dot(n, direction) < zero(t) ? n : -one(t).*n
       for lgh=lights
         vl = normalize(lgh.center .- x)
         t, hat = intersect(x, vl) 
@@ -80,8 +88,10 @@ const spheres = (
       return f
     end
 
+    w, h = 320, 240
+    c = zeros(w, h)
+    ε = Dual(0.,1.)
     open("image.pgm", "w") do f
-      w, h = 320, 240
       cam = (50.0, 50.0, 295.6)
       dir = (0.0, -0.042612, -1.0) |> normalize
       cx  = (0.5135w/h, 0.0, 0.0)
@@ -100,6 +110,7 @@ const spheres = (
               g += raytrace(cam .+ 140.0.*d, normalize(d))[1]
             end
           end
+          #c[x, y] = corrected(g) |> partials
           write(f, corrected(g/4.0))
         end
       end
